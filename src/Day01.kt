7a36dd7cf91056a90bde7e5889bf9c82eefce3db
Tim Miller <tim.miller@e-gineering>
2022-12-12 21:07:08 -0500

    day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var elfFoodItems = input
+        var mostCalories = 0
+        while (elfFoodItems.isNotEmpty()) {
+            val endOfItemsIndex = elfFoodItems.indexOfFirst { it.isBlank() }
+
+            if (endOfItemsIndex != -1) {
+                val calories = elfFoodItems.takeWhile { it.isNotEmpty() }
+                    .sumOf { it.toInt() }
+                if (calories > mostCalories) {
+                    mostCalories = calories
+                }
+                elfFoodItems = elfFoodItems.subList(endOfItemsIndex + 1, elfFoodItems.size)
+            } else {
+                val calories = elfFoodItems.sumOf { it.toInt() }
+                if (calories > mostCalories) {
+                    mostCalories = calories
+                }
+                elfFoodItems = listOf()
+            }
+        }
+        return mostCalories
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +29,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
