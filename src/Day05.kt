a23ee949323fe3260208382b6f4bbf07b5ce4ccc
Tim Miller <tim.miller@e-gineering>
2022-12-16 09:04:50 -0500

    day 5 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -44,13 +44,60 @@ fun main() {
         return topCrates
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): String {
+
+        val indexOfColumnRow = input.indexOfFirst { it.contains("1") }
+
+        val numberOfStacks = input[indexOfColumnRow].trim().takeLast(1).toInt()
+
+        val crateIds = mutableListOf<List<String>>()
+
+        for (row in input.take(indexOfColumnRow)) {
+            val crates = row.chunked(4).map { it.substring(1, 2) }
+            crateIds += crates
+        }
+
+        val stacks = mutableListOf<ArrayDeque<String>>()
+
+        for (stack in 1..numberOfStacks) {
+            stacks += ArrayDeque<String>()
+        }
+
+        for (row in crateIds.reversed()) {
+            for ((stackIndex, crateId) in row.withIndex()) {
+                if (crateId != " ") {
+                    stacks[stackIndex].addLast(crateId)
+                }
+            }
+        }
+
+        val instructions = input.subList(indexOfColumnRow + 2, input.size)
+
+        for (instruction in instructions) {
+
+            val cratesToMove = mutableListOf<String>()
+            for (move in 1..instruction.quantity()) {
+                cratesToMove += stacks[instruction.fromStack() - 1].removeLast()
+            }
+
+            for (crate in cratesToMove.reversed()) {
+                stacks[instruction.toStack() - 1].addLast(crate)
+            }
+        }
+
+        var topCrates = ""
+
+        for (stack in stacks) {
+            topCrates += stack.last()
+        }
+
+        return topCrates
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == "CMZ")
+    check(part2(testInput) == "MCD")
 
     val input = readInput("Day05")
     part1(input).println()
