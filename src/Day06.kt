d2c23a480c0d6a4f8d8533d93854c9a85872d277
Tim Miller <tim.miller@e-gineering>
2022-12-16 18:26:51 -0500

    day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -18,14 +18,33 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        val signal = input[0]
+
+        for (i in 0..signal.length - 14) {
+            val substring = signal.substring(i, i + 14)
+            if (substring.allUnique()) {
+                return i + 14
+            }
+        }
         return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 19)
 
     val input = readInput("Day06")
     part1(input).println()
     part2(input).println()
+}
+
+fun String.allUnique(): Boolean {
+
+    for ((index, item) in this.withIndex()) {
+        if (this.substring(index + 1, this.length).contains(item)) {
+            return false
+        }
+    }
+    return true
 }
