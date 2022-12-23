b6d6b647c7bb0e6d9e5697c28d20e15bfa14406c
Tim Miller <tim.miller@e-gineering>
2022-12-23 16:11:00 -0500

    day 11 part 2
    the key was doing modulo by the product of all the monkey tests.
    this avoided changing the result of their test but kept the
    values from growing overly large.

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,5 +1,3 @@
-import java.math.BigInteger
-
 fun main() {
     fun part1Test(input: List<String>): Int {
         val monkeyData = input.chunked(7)
@@ -154,52 +152,47 @@ fun main() {
 
         val monkeys = listOf(
             BigMonkey(
-                items = mutableListOf(BigInteger.valueOf(79), BigInteger.valueOf(98)),
+                items = mutableListOf(79, 98),
                 operation = { it.times(19) },
                 test = { it.isDivisibleBy(23) },
+                testValue = 23,
                 recipientIfTrue = 2,
                 recipientIfFalse = 3,
             ),
             BigMonkey(
-                items = mutableListOf(
-                    BigInteger.valueOf(54),
-                    BigInteger.valueOf(65),
-                    BigInteger.valueOf(75),
-                    BigInteger.valueOf(74),
-                ),
+                items = mutableListOf(54, 65, 75, 74),
                 operation = { it.plus(6) },
                 test = { it.isDivisibleBy(19) },
+                testValue = 19,
                 recipientIfTrue = 2,
                 recipientIfFalse = 0,
             ),
             BigMonkey(
-                items = mutableListOf(
-                    BigInteger.valueOf(79),
-                    BigInteger.valueOf(60),
-                    BigInteger.valueOf(97),
-                ),
+                items = mutableListOf(79, 60, 97),
                 operation = { it.squared() },
                 test = { it.isDivisibleBy(13) },
+                testValue = 13,
                 recipientIfTrue = 1,
                 recipientIfFalse = 3,
             ),
             BigMonkey(
-                items = mutableListOf(BigInteger.valueOf(74)),
+                items = mutableListOf(74),
                 operation = { it.plus(3) },
                 test = { it.isDivisibleBy(17) },
+                testValue = 17,
                 recipientIfTrue = 0,
                 recipientIfFalse = 1,
             )
         )
 
+        val commonModulo = monkeys.map { it.testValue }
+            .fold(1) { total, i -> total * i }
 
-
-        for (round in 1..1000) {
-            println(round)
+        for (round in 1..10000) {
             for (monkey in monkeys) {
                 for (item in monkey.items) {
                     val worry = monkey.operation(item)
-                    val relief = worry
+                    var relief = worry % commonModulo
                     if (monkey.test(relief)) {
                         monkeys[monkey.recipientIfTrue].items += relief
                     } else {
@@ -220,8 +213,102 @@ fun main() {
         return monkeyBusiness
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val monkeyData = input.chunked(7)
+
+        val monkeys = listOf(
+            BigMonkey(
+                items = mutableListOf(57, 58),
+                operation = { it.times(19) },
+                test = { it.isDivisibleBy(7) },
+                testValue = 7,
+                recipientIfTrue = 2,
+                recipientIfFalse = 3,
+            ),
+            BigMonkey(
+                items = mutableListOf(66, 52, 59, 79, 94, 73),
+                operation = { it.plus(1) },
+                test = { it.isDivisibleBy(19) },
+                testValue = 19,
+                recipientIfTrue = 4,
+                recipientIfFalse = 6,
+            ),
+            BigMonkey(
+                items = mutableListOf(80),
+                operation = { it.plus(6) },
+                test = { it.isDivisibleBy(5) },
+                testValue = 5,
+                recipientIfTrue = 7,
+                recipientIfFalse = 5,
+            ),
+            BigMonkey(
+                items = mutableListOf(82, 81, 68, 66, 71, 83, 75, 97),
+                operation = { it.plus(5) },
+                test = { it.isDivisibleBy(11) },
+                testValue = 11,
+                recipientIfTrue = 5,
+                recipientIfFalse = 2,
+            ),
+            BigMonkey(
+                items = mutableListOf(55, 52, 67, 70, 69, 94, 90),
+                operation = { it.squared() },
+                test = { it.isDivisibleBy(17) },
+                testValue = 17,
+                recipientIfTrue = 0,
+                recipientIfFalse = 3,
+            ),
+            BigMonkey(
+                items = mutableListOf(69, 85, 89, 91),
+                operation = { it.plus(7) },
+                test = { it.isDivisibleBy(13) },
+                testValue = 13,
+                recipientIfTrue = 1,
+                recipientIfFalse = 7,
+            ),
+            BigMonkey(
+                items = mutableListOf(75, 53, 73, 52, 75),
+                operation = { it.times(7) },
+                test = { it.isDivisibleBy(2) },
+                testValue = 2,
+                recipientIfTrue = 0,
+                recipientIfFalse = 4,
+            ),
+            BigMonkey(
+                items = mutableListOf(94, 60, 79),
+                operation = { it.plus(2) },
+                test = { it.isDivisibleBy(3) },
+                testValue = 3,
+                recipientIfTrue = 1,
+                recipientIfFalse = 6,
+            )
+        )
+
+        val commonModulo = monkeys.map { it.testValue }
+            .fold(1) { total, i -> total * i }
+
+        for (round in 1..10000) {
+            for (monkey in monkeys) {
+                for (item in monkey.items) {
+                    val worry = monkey.operation(item)
+                    var relief = worry % commonModulo
+                    if (monkey.test(relief)) {
+                        monkeys[monkey.recipientIfTrue].items += relief
+                    } else {
+                        monkeys[monkey.recipientIfFalse].items += relief
+                    }
+
+                }
+                monkey.inspections += monkey.items.size
+                monkey.items.clear()
+            }
+        }
+
+        val monkeyBusiness = monkeys.map { it.inspections }
+            .sortedDescending()
+            .take(2)
+            .fold(1L) { total, i -> total * i }
+
+        return monkeyBusiness
     }
 
     // test if implementation meets criteria from the description, like:
@@ -244,14 +331,14 @@ data class Monkey(
 )
 
 data class BigMonkey(
-    val items: MutableList<BigInteger>,
-    val operation: (old: BigInteger) -> BigInteger,
-    val test: (BigInteger) -> Boolean,
+    val items: MutableList<Long>,
+    val operation: (old: Long) -> Long,
+    val test: (Long) -> Boolean,
+    val testValue: Int,
     val recipientIfTrue: Int,
     val recipientIfFalse: Int,
     var inspections: Int = 0,
 )
-
 
 fun Int.squared() = this * this
 
@@ -260,12 +347,4 @@ fun Int.isDivisibleBy(int: Int) = this % int == 0
 fun Long.squared() = this * this
 
 fun Long.isDivisibleBy(long: Long) = this % long == 0L
-
-fun BigInteger.times(long: Long): BigInteger = this.multiply(BigInteger.valueOf(long))
-
-fun BigInteger.isDivisibleBy(long: Long) = this.mod(BigInteger.valueOf(long)) == BigInteger.valueOf(0)
-
-fun BigInteger.plus(long: Long) = this.plus(BigInteger.valueOf(long))
-
-fun BigInteger.squared(): BigInteger = this.multiply(this)
 
